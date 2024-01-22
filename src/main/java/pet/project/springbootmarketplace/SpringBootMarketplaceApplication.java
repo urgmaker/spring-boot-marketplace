bd409f4fa0090c51a686b3c204fd3fa2fe028ba3
Уранов Роман <roman.uranov@gazprombank.ru>
2024-01-22 15:33:53 +0300

    Initial commit

---
+++ src/main/java/pet/project/springbootmarketplace/SpringBootMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package pet.project.springbootmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pet/project/springbootmarketplace/SpringBootMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.springbootmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
